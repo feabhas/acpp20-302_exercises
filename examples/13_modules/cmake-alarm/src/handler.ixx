99435e72aec6740a80d580ddb7b453c20de76619
Martin Bond <28291143+martinbond7@users.noreply.github.com>
2024-10-15 15:47:18 +0100

    Updated module build files for revised cmake-project templates

--- examples/13_modules/cmake-alarm/src/handler.ixx
+++
@@ -1,7 +0,0 @@
-export module Handler;
-
-export import Alarms;
-
-export namespace Handler {
-  void cancel(typename Alarms::Alarm& alarm);
-}
--- exercises/solutions/10_modules/src/main.cpp
+++ exercises/solutions/10_modules/src/main.cpp
@@ -14,13 +14,13 @@ using DataLogger::MeasurementsContainer;
 
 int main() {
   Measurements<Measurement> base {"base station", MeasurementType::temperature};
-  // for (auto m : {21.1, 18.7, 19.6, 22.3, 20.1}) {
-  //   base.add(m);
-  // }
+  for (auto m : {21.1, 18.7, 19.6, 22.3, 20.1}) {
+    base.add(m);
+  }
 
-  // for (auto measurements = base.data(); auto& m: measurements) {
-  //   std::cout << m.get_value() << ' ';
-  // }
-  // std::cout << '\n';
+  for (auto measurements = base.data(); auto& m: measurements) {
+    std::cout << m.get_value() << ' ';
+  }
+  std::cout << '\n';
 }
 
